aa6a4444214d0b30b05a90abde93c0326b2b9b34
KIRUBAKARAN K <kirubakaran02002@gmail.com>
2024-08-26 23:32:29 +0530

    verify OTP and navigate to password screen

--- app/src/main/java/com/example/can_i_serve/Activity/OTPVerificationActivity.kt
+++ app/src/main/java/com/example/can_i_serve/Activity/OTPVerificationActivity.kt
@@ -9,15 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.can_i_serve.databinding.AlertDialogeBinding
 import com.example.can_i_serve.databinding.OtpScreenActivityBinding
 import com.example.can_i_serve.utils
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseException
-import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
-import com.google.firebase.firestore.FirebaseFirestore
-import java.io.Serializable
 import java.util.concurrent.TimeUnit
 
 class OTPVerificationActivity: AppCompatActivity() {
@@ -29,6 +28,9 @@ class OTPVerificationActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=OtpScreenActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.root.setOnClickListener {
+            utils.hideKeyboard(it,this)
+        }
 
         mAuth=FirebaseAuth.getInstance()
         val data:RegisterDetails = intent.getSerializableExtra("register_details") as RegisterDetails
@@ -39,28 +41,26 @@ class OTPVerificationActivity: AppCompatActivity() {
 //        val value: MutableMap<String, String> = HashMap()
 //        FirebaseFirestore.getInstance().collection("test").add(value)
         binding.verifyBtn.setOnClickListener {
-            verifyOtp()
+            val otp=binding.otp1.text.toString()+binding.otp2.text+binding.otp3.text+binding.otp4.text+binding.otp5.text+binding.otp6.text
+            val credentiale=PhoneAuthProvider.getCredential(verificationCode,otp)
+            verifyOtp(credentiale)
         }
 
     }
 
-    private fun verifyOtp() {
-        val realOTP=verificationCode.substring(0,4)
-        val otp1=binding.otp1.text.toString()
-        val otp2 =binding.otp2.text
-        val otp3= binding.otp3.text
-        val otp4=binding.otp4.text
-        val otp=otp1+otp2+otp3+otp4
-        Log.d("##OTP","$otp ${verificationCode}")
-
-        if(otp==realOTP){
-            val intent= Intent(this,PasswordActivity::class.java)
-            startActivity(intent)
-            finish()
-        }else{
-            Log.d("##OTP","$otp ${verificationCode.substring(0,4)}")
-            alert("Enter valid OTP")
-        }
+    private fun verifyOtp(credentiale: PhoneAuthCredential) {
+        mAuth.signInWithCredential(credentiale).addOnCompleteListener( object :
+            OnCompleteListener<AuthResult> {
+            override fun onComplete(task: Task<AuthResult>) {
+               if(task.isSuccessful){
+                   val intent= Intent(this@OTPVerificationActivity,PasswordActivity::class.java)
+                   startActivity(intent)
+                   finish()
+               }else{
+                   utils.toastMessage(this@OTPVerificationActivity,"OTP verification failed")
+               }
+            }
+        })
     }
 
     private fun alert(message: String) {
@@ -87,6 +87,7 @@ class OTPVerificationActivity: AppCompatActivity() {
             .setActivity(this)         // Make sure to set the current activity
             .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
                 override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+//                    verifyOtp(credentiale)
                     utils.toastMessage(this@OTPVerificationActivity, "Verification Completed")
                 }
 
--- app/src/main/java/com/example/can_i_serve/Activity/PasswordActivity.kt
+++ app/src/main/java/com/example/can_i_serve/Activity/PasswordActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.can_i_serve.databinding.OtpScreenActivityBinding
 import com.example.can_i_serve.databinding.PasswordScreenActivityBinding
+import com.example.can_i_serve.utils
 
 class PasswordActivity:AppCompatActivity() {
     private lateinit var binding: PasswordScreenActivityBinding
@@ -12,8 +13,19 @@ class PasswordActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=PasswordScreenActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.root.setOnClickListener {
+            utils.hideKeyboard(it,this)
+        }
 
         binding.registerBtn.setOnClickListener {
+           verifyPassword()
+        }
+    }
+
+    private fun verifyPassword() {
+        val password=binding.password.text
+        val confirmPassword=binding.confirmPassword.text
+        if(password==confirmPassword){
             val intent= Intent(this,LoginActivity::class.java)
             startActivity(intent)
             finish()
